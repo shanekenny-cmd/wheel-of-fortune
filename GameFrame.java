77ad3e4d704254c97a44c9be878245cb2dbe74b9
shanekenny-cmd <57236017+shanekenny-cmd@users.noreply.github.com>
2021-10-13 14:20:40 -0400

    Add files via upload
    These changes create a 600px by 600px window to be used when graphics are being implemented in later iterations. Also adds more in-depth header to each file.

--- GameFrame.java
+++ GameFrame.java
@@ -1,4 +1,23 @@
 /* GameFrame.java
+ * Shane Kenny, Israel Perea, Kyle Holzshu
+ * CSC381: Software Engineering
+ * This is the Game Frame class - opens and formats the window of the game
  */
 
- 
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	GameFrame() {
+
+		this.add(new GamePanel());
+		this.setTitle("Wheel Of Fortune");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+
+	}
+
+}
