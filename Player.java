af06bc6f287ffd4fc067032c677912406be5870b
pereai1 <91912450+pereai1@users.noreply.github.com>
2021-10-20 13:05:17 -0400

    Create Player.java

--- Player.java
+++ Player.java
@@ -2,3 +2,67 @@
  */
 
  
+public class Player
+{
+    private double banked;                                                                     
+    private double balance;                                                             
+    private boolean skipTurn;                                                   
+     
+
+    // no-argument constructor
+    public Player ()
+    {
+
+
+    }
+
+    // initialization constructor
+    public Player (double bK, double b, boolean sT)
+    {
+        banked = bK;
+        balance = b;
+		skipTurn = sT;
+
+    }
+
+    
+    // mutator method for banked
+    public void setBanked (double bK)
+    {
+        banked = bK;
+
+    }
+
+    // mutator method for balance
+    public void setBalance (double b)
+    {
+        balance = b;
+
+    }
+
+    // mutator method for skip turn
+    public void setSkipT (boolean sT)
+    {
+        skipTurn = sT;
+
+    }
+
+    // accessor method for banked
+    public int getBanked()
+    {
+        return banked;
+
+    }
+
+    // accessor method for balance
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    // accessor method for skip turn
+    public double getSkipTurn()
+    {
+        return annualInterestRate;
+
+    }
